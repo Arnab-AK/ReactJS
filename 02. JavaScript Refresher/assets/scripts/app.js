a15981bcf75ad0b85946c359222f486890bc2ad5
Arnab-AK <royjason715@gmail.com>
2024-04-24 22:17:15 +0530

    Section 2 - "import" & "export"

---
+++ 02. JavaScript Refresher/assets/scripts/app.js
@@ -0,0 +1,8 @@
+// import { apiKey } from "./utils.js";
+// import apiKey from "./utils.js";
+// import { apiKey, abc } from "./utils.js";
+// import * as util from "./utils.js";
+import { apiKey, abc as content } from "./utils.js";
+
+// console.log(util.apiKey, util.abc, util.default);
+console.log(content);
---
+++ 02. JavaScript Refresher/assets/scripts/utils.js
@@ -0,0 +1,4 @@
+// export let apiKey = "aslkdnlksnlblsal1asdn1";
+export default "alklaksndnaoij1n2n";
+export let apiKey = "aslkdnlksnlblsal1asdn1";
+export let abc = "aslkdnlksnlsasjdasm";
