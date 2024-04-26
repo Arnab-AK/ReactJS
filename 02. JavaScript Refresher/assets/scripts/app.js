a0c3507c4fbd663b4b799f27a33ba8622445d59c
Arnab-AK <royjason715@gmail.com>
2024-04-26 21:39:53 +0530

    Revisiting Variables & Values

--- 02. JavaScript Refresher/assets/scripts/app.js
+++ 02. JavaScript Refresher/assets/scripts/app.js
@@ -2,7 +2,12 @@
 // import apiKey from "./utils.js";
 // import { apiKey, abc } from "./utils.js";
 // import * as util from "./utils.js";
-import { apiKey, abc as content } from "./utils.js";
+// import { apiKey, abc as content } from "./utils.js";
 
 // console.log(util.apiKey, util.abc, util.default);
-console.log(content);
+// console.log(content);
+
+let userMessage = 'Hi, how are you ?'
+
+console.log("Hello World!");
+console.log(userMessage);
