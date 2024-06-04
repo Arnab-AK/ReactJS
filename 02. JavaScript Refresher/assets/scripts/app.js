6381a33285e6be8d59f9d2f019ce37d5df5d6b06
Arnab-AK <royjason715@gmail.com>
2024-06-04 22:04:01 +0530

    Coding Exercise 1 : Working with Functions

--- 02. JavaScript Refresher/assets/scripts/app.js
+++ 02. JavaScript Refresher/assets/scripts/app.js
@@ -1,13 +1,45 @@
-// import { apiKey } from "./utils.js";
-// import apiKey from "./utils.js";
-// import { apiKey, abc } from "./utils.js";
-// import * as util from "./utils.js";
-// import { apiKey, abc as content } from "./utils.js";
+/*
+import { apiKey } from "./utils.js";
+import apiKey from "./utils.js";
+import { apiKey, abc } from "./utils.js";
+import * as util from "./utils.js";
+import { apiKey, abc as content } from "./utils.js";
 
-// console.log(util.apiKey, util.abc, util.default);
-// console.log(content);
+console.log(util.apiKey, util.abc, util.default);
+console.log(content);
 
 let userMessage = 'Hi, how are you ?'
 
 console.log("Hello World!");
 console.log(userMessage);
+
+console.log(10 === 10);
+if (10 === 10){
+    console.log("Works!");
+}
+
+function createGreeting(userName, message = 'How are you') {
+    console.log("Hello World!");
+    console.log(userName);
+    console.log(message);
+    return 'Hi, I am ' + userName + ', ' + message;
+}
+
+const greetings1 = createGreeting("Maxine")
+console.log(greetings1);
+
+console.log(createGreeting("Max"));
+*/
+
+
+/*
+Coding Exercise
+
+Coding Exercise 1
+
+function combine(x, y, z) {
+   const res = x * y / z;
+   return res;
+}
+
+*/
